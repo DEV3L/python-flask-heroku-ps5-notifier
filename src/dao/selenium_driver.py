07377c6684e8950c76b3e77f3c1bd2b30a63c6a2
Justin Beall <jus.beall@gmail.com>
2020-11-29 14:25:41 -0500

    Fix chrome driver path and google chrome bin variables

--- src/dao/selenium_driver.py
+++ src/dao/selenium_driver.py
@@ -3,8 +3,10 @@ import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+CHROMEDRIVER_PATH = os.environ.get("CHROMEDRIVER_PATH",
+                                   "/Users/dev3l/workspace/python-flask-heroku-ps5-notifier/drivers/chromedriver")
 GOOGLE_CHROME_BIN = os.environ.get("GOOGLE_CHROME_BIN",
-                                   '/Users/dev3l/workspace/python-flask-heroku-ps5-notifier/drivers/chromedriver')
+                                   '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome')
 IS_HEADLESS = os.environ.get("HEADLESS", "True")
 USER_AGENT = os.environ.get("USER_AGENT",
                             "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) "
@@ -17,13 +19,14 @@ class SeleniumDriver:
         is_headless = IS_HEADLESS == "True"
 
         options.headless = is_headless
+        options.binary_location = GOOGLE_CHROME_BIN
         options.add_argument("--start-maximized")
         options.add_argument("--kiosk")
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--no-sandbox")
 
         self.options = options
-        self.driver = webdriver.Chrome(options=options, executable_path=GOOGLE_CHROME_BIN)
+        self.driver = webdriver.Chrome(options=options, executable_path=CHROMEDRIVER_PATH)
 
     def quit(self):
         self.driver.quit()
