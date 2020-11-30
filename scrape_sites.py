e7de805fa9c7b532ca47520d2efb3d2285c2a5eb
Justin Beall <jus.beall@gmail.com>
2020-11-30 09:31:02 -0500

    Add some error handling around scrape sites

--- scrape_sites.py
+++ scrape_sites.py
@@ -1,6 +1,5 @@
 from dotenv import load_dotenv
 
-
 load_dotenv()
 
 from src.scrapers.scraper_amazon import ScraperAmazon
@@ -14,7 +13,7 @@ from src.dao.selenium_driver import SeleniumDriver
 logger = LoggingService('scrape_sites')
 
 
-def check_site(scraper):
+def check_site(scraper, twilio_service):
     if scraper.is_ps5_console_available():
         logger.info(f'PS5 Console AVAILABLE at {scraper.site_name}')
         twilio_service.send_message(f'PS5 Console Available at {scraper.site_name} - {scraper.ps5_console_url}')
@@ -28,20 +27,31 @@ def check_site(scraper):
         logger.info(f'PS5 Digital not available at {scraper.site_name}')
 
 
-if __name__ == "__main__":
-    twilio_service = TwilioService()
-    selenium_driver = SeleniumDriver()
-
+def run(selenium_driver, twilio_service):
     scraper_best_buy = ScraperBestBuy(selenium_driver)
-    check_site(scraper_best_buy)
+    check_site(scraper_best_buy, twilio_service)
 
     scraper_amazon = ScraperAmazon(selenium_driver)
-    check_site(scraper_amazon)
+    check_site(scraper_amazon, twilio_service)
 
     scraper_game_stop = ScraperGameStop(selenium_driver)
-    check_site(scraper_game_stop)
+    check_site(scraper_game_stop, twilio_service)
 
     scraper_target = ScraperTarget(selenium_driver)
-    check_site(scraper_target)
+    check_site(scraper_target, twilio_service)
 
     selenium_driver.quit()
+
+
+if __name__ == "__main__":
+    selenium_driver = SeleniumDriver()
+
+    try:
+        twilio_service = TwilioService()
+        selenium_driver = SeleniumDriver()
+
+        run(selenium_driver, twilio_service)
+    except Exception as e:
+        logger.exception(e)
+    finally:
+        selenium_driver.quit()
